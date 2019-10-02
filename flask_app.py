6076de8a9da1375055fee588f7b21ea2ff6a2663
Jack <jack.mckenzie@bbc.co.uk>
2019-10-02 15:57:35 +0100

    third push

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,7 @@
 # flask_app.py
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
+#import requests
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,3 +10,7 @@ app.config["DEBUG"] = True
 @app.route("/")
 def hello():
     return "Hello World! This is an edit!"
+
+@app.route("/api")
+def hello_2():
+    return jsonify(pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]}).to_dict(orient='records'))
